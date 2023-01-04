8e14b0afd0be70608522f00799a47bed66fa513c
9kin <9kin.github@mail.ru>
2023-01-04 15:37:34 +0300

    add workflow scripts

---
+++ deploy_server.py
@@ -0,0 +1,18 @@
+# usage : uvicorn deploy_server:app --reload --host 0.0.0.0
+from fastapi import FastAPI, HTTPException
+from os import system
+
+app = FastAPI()
+PY = 'python3.10'
+HASH = ''
+
+def validate(owner, repo, hash):
+    return owner == '9kin' and repo == 'is_tex' and hash == HASH
+
+@app.get("/pull/{owner}/{repo}/{pull_request_number}")
+def pull(owner: str, repo: str, pull_request_number: int, hash: str):
+    if not validate(owner, repo, hash):
+        raise HTTPException(status_code=404, detail='(')
+    system(f'{PY} pull.py {owner} {repo} {pull_request_number} &')
+    return "ok"
+
---
+++ pull.py
@@ -0,0 +1,81 @@
+import sys
+from os import system, chdir
+import requests
+import ntpath
+import mimetypes
+from pathlib import Path
+
+gh_token = ''
+catbox_token = ''
+
+OWNER, REPO, PULL_REQUEST_NUMBER = sys.argv[1:]
+REPO_URL = f'https://github.com/{OWNER}/{REPO}'
+
+
+def upload_file(file_path):
+    with open(file_path, 'rb') as file:
+        file_content = file.read()
+        file_name = ntpath.basename(file_path)
+        file_type = mimetypes.guess_type(file_path)[0]
+        file_info = file_name, file_content, file_type
+
+    params = {
+        'reqtype': 'fileupload',
+        'userhash': catbox_token
+    }
+    files = {
+        'fileToUpload': file_info
+    }
+    result = requests.post('https://catbox.moe/user/api.php', data=params, files=files)
+    if result.status_code != 200:
+        print('ERROR while uploading file on catbox (file hosting)')
+        exit(-1)
+    return result.text
+
+
+def comment(body):
+    requests.post(f'https://api.github.com/repos/{OWNER}/{REPO}/issues/2/comments', headers={
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {gh_token}",
+        "X-GitHub-Api-Version": "2022-11-28"},
+                  json={"body": body}
+                  )
+
+
+system(f'rm -rf {REPO}')
+system(f'rm -rf {REPO}_pull')
+system(f'git clone {REPO_URL}')
+system(f'cp -R {REPO} {REPO}_pull')
+chdir(f'{REPO}_pull')
+system(f'git pull origin pull/{PULL_REQUEST_NUMBER}/head')
+chdir(f'..')
+
+dependencies = {
+    '.tex': 'base.tex'
+}
+
+tex_files = set()
+for obj in requests.get(f'https://api.github.com/repos/{OWNER}/{REPO}/pulls/{PULL_REQUEST_NUMBER}/files', headers={
+    "Accept": "application/vnd.github+json",
+    "Authorization": f"Bearer {gh_token}",
+    "X-GitHub-Api-Version": "2022-11-28"}).json():
+    file_path = obj['filename']
+    if file_path in dependencies:
+        system(
+            f'latexdiff {REPO}/{file_path} {REPO}_pull/{file_path} > tmp')
+        system(f'cat tmp > {REPO}/{file_path}')
+        tex_files.add(dependencies[file_path])
+
+chdir(REPO)
+urls = []
+for base in tex_files:
+    system(
+        f'latexdiff {base} {base} > tmp')
+    system(f'cat tmp > {base}')
+    for _ in range(2):
+        system(f'pdflatex -synctex=1 -interaction=nonstopmode --shell-escape {base}')
+    name = Path(base).with_suffix('')
+    urls.append(f'[{name}.pdf]({upload_file(f"{name}.pdf")})')
+
+comment('Изменённые файлы:' + ','.join(urls))
+
